5bfe1133a8eba988831ba6c39959e69544b5f0cf
George Schena <george@madetech.com>
2020-03-31 01:32:49 +0100

    Create method to fetch data

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,5 +1,12 @@
 class PropertiesController < ApplicationController
   def index
-    
+    @properties = get_all_properties
   end
+
+  private
+
+  def get_all_properties
+    'Propertiessssss will be called here'
+  end
+
 end
