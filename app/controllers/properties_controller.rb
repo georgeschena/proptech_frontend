7cc5dd30b7dc194dfd05350256e70c86b179b572
George Schena <george@madetech.com>
2020-03-31 18:46:50 +0100

    Rename method to reflect fetching only todays properties

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,13 +2,13 @@ require "net/http"
 
 class PropertiesController < ApplicationController
   def index
-    @properties = get_all_properties
+    @properties = get_todays_properties
   end
 
   private
 
-  def get_all_properties
-    url = "https://planning-portal-scraper-api.herokuapp.com/all_properties"
+  def get_todays_properties
+    url = "https://planning-portal-scraper-api.herokuapp.com/todays_properties"
 
     response = Net::HTTP.get_response(URI.parse(url))
     data = JSON.parse(response.body)
