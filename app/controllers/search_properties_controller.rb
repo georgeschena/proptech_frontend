d893ada87658badf05edd32846c853ed5cb3f09a
George Schena <george@madetech.com>
2020-04-02 16:14:34 +0100

    use.ssl to be true

--- app/controllers/search_properties_controller.rb
+++ app/controllers/search_properties_controller.rb
@@ -15,7 +15,7 @@ class SearchPropertiesController < ApplicationController
   def parse_search_response
     uri = URI.parse("https://planning-portal-scraper-api.herokuapp.com/search_properties")
     https = Net::HTTP.new(uri.host, uri.port)
-    https.use_ssl = false
+    https.use_ssl = true
     req = Net::HTTP::Post.new(uri.path)
     req['Content-Type'] = 'application/json'
 
